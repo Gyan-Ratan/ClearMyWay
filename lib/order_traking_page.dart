78ef29aa0773c5e7cca2a816b20e62b2664d4456
Kanishak Chaurasia <kanishakchaurasia2@gmail.com>
2022-08-21 09:11:41 +0530

    Revert "Added Polylines"
    This reverts commit 2ed79810598a679330db484d8fa30cbdc87c9016

--- lib/order_traking_page.dart
+++ lib/order_traking_page.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
-import 'package:google_mao/constants.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class OrderTrackingPage extends StatefulWidget {
@@ -18,6 +16,32 @@ class OrderTrackingPageState extends State<OrderTrackingPage> {
   static const LatLng sourceLocation = LatLng(37.33500926, -122.03272188);
   static const LatLng destination = LatLng(37.33429383, -122.06600055);
 
+<<<<<<< HEAD
+  List<LatLng> polylineCoordinates =[];
+
+  void getPolyPoints() async {
+    PolylinePoints polylinePoints = PolylinePoints();
+
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        // GOOGLE_API_KEY = "${GOOGLE_API_KEY},
+        google_api_key,
+        PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
+        PointLatLng(destination.latitude, destination.longitude)
+    );
+    if (result.points.isNotEmpty) {
+      for (var point in result.points) {
+        polylineCoordinates.add(
+            LatLng(point.latitude,point.longitude),
+          );
+        setState(() {});
+      }}
+  }
+  @override
+  void initState(){
+    getPolyPoints();
+    super.initState();
+  }
+||||||| 2ed7981... Added Polylines
   List<LatLng> polylineCoordinates =[];
 
   void getPolyPoints() async {
@@ -41,6 +65,8 @@ class OrderTrackingPageState extends State<OrderTrackingPage> {
     getPolyPoints();
     super.initState();
   }
+=======
+>>>>>>> parent of 2ed7981... Added Polylines
 
 
   @override
@@ -52,30 +78,11 @@ class OrderTrackingPageState extends State<OrderTrackingPage> {
           style: TextStyle(color: Colors.black, fontSize: 16),
         ),
       ),
-      body: GoogleMap(
-        initialCameraPosition: const CameraPosition(
+      body: const GoogleMap(
+        initialCameraPosition: CameraPosition(
           target: sourceLocation,
           zoom: 12.5,
         ),
-        polylines: {
-          Polyline(
-            polylineId: PolylineId("route"),
-            points: polylineCoordinates,
-            color: primaryColor,
-            width: 6,
-          ),
-        },
-        markers: {
-          const Marker(markerId: MarkerId("source"),
-          position: sourceLocation,
-          ),
-          const Marker(markerId: MarkerId("destination"),
-            position: destination
-            ,
-          ),
-        },
-
-
       ),
     );
   }
